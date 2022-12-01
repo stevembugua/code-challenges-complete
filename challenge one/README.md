2dc270379c72411bf84af6c234b7fe926a30107c
mbugua <mbuguafaimba@gmail.com>
2022-12-01 15:26:30 +0300

    completed challenges

---
+++ challenge one/README.md
@@ -0,0 +1,3 @@
+The program is about grading students marks.
+It takes the stdents marks and assigns a letter depending on where the marks lie.
+An example is if a student scores above 79 marks it assigns a grade of "A" and so forth depending on the range given
---
+++ challenge one/index.js
@@ -0,0 +1,24 @@
+const gradeInput = document.querySelector(".grade");
+const calculateGradeButton = document.querySelector(".calcBtn");
+
+calculateGradeButton.addEventListener("click", () => {
+  calculateStudentGrade(parseInt(gradeInput.value, 10));
+});
+
+const calculateStudentGrade = (grade) => {
+  if (!(grade < 0 || grade > 100)) {
+    if (grade > 79 && grade <= 100) {
+      alert("A");
+    } else if (grade >= 60 && grade <= 79) {
+      alert("B-");
+    } else if (grade >= 50 && grade <= 59) {
+      alert("C-");
+    } else if (grade >= 40 && grade <= 49) {
+      alert("D-");33
+    } else if (grade < 40) {
+      alert("E");
+    }
+  } else {
+    alert("Invalid Grade. Enter a grade value between 0 and 100");
+  }
+};
